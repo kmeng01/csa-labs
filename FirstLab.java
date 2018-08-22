5b41ae8b2a8a7c602e4e3b13a52a12c25a8c4467
Kevin Meng <kevin.meng.01@gmail.com>
2018-08-21 19:27:59 -0500

    add face functionality

--- FirstLab.java
+++ FirstLab.java
@@ -32,8 +32,27 @@ public class FirstLab
 	public static void bonus()
 	{
 		// Write the code HERE to display a silly face or picture
-		System.out.println("\n+--------------------+");
-		
+		System.out.println("\n+--------------------------------------------+");
+		System.out.println("|                HAPPY MOON                  |");
+		System.out.println("|                ____....                    |");
+		System.out.println("|               a#####~:::::::,              |");
+		System.out.println("|           a######P\";:::::::::::,           |");
+		System.out.println("|        a########:::::::::::::::::,         |");
+		System.out.println("|       ########P::::::::::::*:::::::        |");
+		System.out.println("|      ########P::::::::::::::::::.:::.      |");
+		System.out.println("|     ##### ##P:::::::::::::::::::::::;.     |");
+		System.out.println("|    .#### O ##:::::*:::::::::::::::.::;.    |");
+		System.out.println("|    ###### #### ::::::::::::::::::.:::::    |");
+		System.out.println("|    ########@###,::::::::::::::::::::::;    |");
+		System.out.println("|    #########~~~:::::::::::::::*:::.:::;    |");
+		System.out.println("|     ##### ##:::::::::::::::::::::::::;     |");
+		System.out.println("|      ####a__ay::::::::::::::::::::::;      |");
+		System.out.println("|       ########;::::::::::::::::::::;       |");
+		System.out.println("|        ########a::::::::::::::::::'        |");
+		System.out.println("|          ########.:::::::::*;:::'          |");
+		System.out.println("|           `d######a.::::::::::'            |");
+		System.out.println("|              `~9#####.::::''               |");
+		System.out.println("+--------------------------------------------+");
 		
 	}
 }
