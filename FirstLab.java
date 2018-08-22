eecaf372e773d65bf2abb283095f4ebccd2ea06c
Kevin Meng <kevin.meng.01@gmail.com>
2018-08-21 19:03:10 -0500

    Initial commit

---
+++ FirstLab.java
@@ -0,0 +1,39 @@
+/** 
+ * The FirstLab class prints the author's name & home address, accompanied by a silly face.
+ *
+ * @author Kevin Meng
+ * Collaborators: None
+ * Teacher Name: Mrs. Ishman
+ * Period: 7
+ * Due Date: 08/22/2018
+ */
+
+import java.util.Scanner;
+
+public class FirstLab 
+{
+	public static void main(String[] args) 
+	{
+		// Write the code HERE to print your name and address in boxes
+		System.out.println("+====================+");
+		System.out.println("|    \"Kevin Meng\"    |");
+		System.out.println("+====================+");
+		System.out.println("|  4721 De Grey Ln.  |");
+		System.out.println("+====================+");
+		System.out.println("| Plano, Texas 75093 |");
+		System.out.println("+====================+");
+		
+		// This calls the bonus method to draw your silly face/picture
+		bonus();
+	}
+	
+	/** Draws a silly face or picture
+	*/	
+	public static void bonus()
+	{
+		// Write the code HERE to display a silly face or picture
+		System.out.println("\n+--------------------+");
+		
+		
+	}
+}
