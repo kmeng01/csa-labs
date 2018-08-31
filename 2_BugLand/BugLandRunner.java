7ec34f584bc6702103846a67fe9de7022c7b8bab
Kevin Meng <kevin.meng.01@gmail.com>
2018-08-31 17:40:00 -0500

    complete main functionality - 100 points

--- 2_BugLand/BugLandRunner.java
+++ 2_BugLand/BugLandRunner.java
@@ -9,6 +9,7 @@
  */
 
 import java.awt.Color;
+import java.util.Random;
 import info.gridworld.grid.Location;
 import info.gridworld.actor.ActorWorld;
 import info.gridworld.actor.Bug;
@@ -18,13 +19,16 @@ public class BugLandRunner
 {
 	public static void main(String[] args)
 	{
+		// Create Random and ActorWorld objects necessary for execution of program
+
+		Random rand = new Random();
 		ActorWorld world = new ActorWorld();
 		
 		// Initialize arrays with information for each bug
 		
 		Location[] bugLocs = {new Location(0,4), new Location(5,0), new Location(9,5), new Location(4,9)};
 		Color[] bugColors = {Color.RED, Color.BLUE, Color.GREEN, Color.MAGENTA};
-		int[] bugDirs = {-45, 45, 135, 225};
+		int[] bugDirs = {135, 45, -45, 225};
 		
 		// Add all bugs to world in specifically designated locations, bugColors, and directions
 		for (int i=0; i<4; i++)
@@ -34,16 +38,23 @@ public class BugLandRunner
 			world.add(bugLocs[i], tempBug);
 		}
 		
-		// Fill in rocks at designated positions with different colors
+		// Define matrix describing rock locations
+
+		int[][] rockPos = {{5, -1, -1, 1}, {-1, 4, 1, 1}, {4, 10, 1, -1}, {10, 5, -1, -1}};
+
+		// Fill in rocks at designated positions with different randomized colors
 		
-		int curRow = 4, curCol = 0;
 		for (int i=0; i<4; i++)
 		{
-			Rock tempRock = new Rock();
+			for (int j=0; j<4; j++)
+			{
+				float r = rand.nextFloat(), g = rand.nextFloat(), b = rand.nextFloat();
+				Rock tempRock = new Rock(new Color(r, g, b));
+				world.add(new Location(rockPos[i][0] += rockPos[i][2], rockPos[i][1] += rockPos[i][3]), tempRock);
+			}
 		}
 		
-		
-		// show the world - DO NOT DELETE THIS!!!!
+		// Show the world in GUI
 		world.show();
 	}
 }
