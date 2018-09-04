30d7ed12e66384e53aebf5c4f4e40438315eef5f
Kevin Meng <kevin.meng.01@gmail.com>
2018-09-04 15:46:33 -0500

    update spacing/formatting lab2

--- 2_BugLand/BugLandRunner.java
+++ 2_BugLand/BugLandRunner.java
@@ -10,7 +10,7 @@
  */
 
 import java.awt.Color;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.ArrayList;
 import info.gridworld.grid.Location;
 import info.gridworld.actor.ActorWorld;
@@ -21,20 +21,17 @@ public class BugLandRunner
 {
 	public static void main(String[] args)
 	{
-		// Create Random and ActorWorld objects necessary for execution of program
-
-		Random rand = new Random();
 		ActorWorld world = new ActorWorld();
 		
-		// Create ArrayLists to store all bugs and rocks
+		// Create ArrayLists to store all bug and rock objects for future edits/actions
 
 		ArrayList<Bug> bugList = new ArrayList<Bug>();
 		ArrayList<Rock> rockList = new ArrayList<Rock>();
 
-		// Initialize arrays with information for each bug
+		// Initialize arrays containing information for each bug
 		
-		Location[] bugLocs = {
-			new Location(0, 4), new Location(5, 0), new Location(9, 5), new Location(4, 9)};
+		Location[] bugLocs = {new Location(0, 4), new Location(5, 0), new Location(9, 5),
+			new Location(4, 9)};
 		Color[] bugColors = {Color.RED, Color.BLUE, Color.GREEN, Color.MAGENTA};
 		int[] bugDirs = {135, 45, -45, 225};
 		
@@ -44,11 +41,13 @@ public class BugLandRunner
 		{
 			Bug tempBug = new Bug(bugColors[i]);
 			tempBug.setDirection(bugDirs[i]);
+			
 			world.add(bugLocs[i], tempBug);
+			
 			bugList.add(tempBug);
 		}
 		
-		// Define matrix describing rock locations
+		// Define matrix describing rock locations: (start_row, start_col, delta_row, delta_col)
 
 		int[][] rockPos = {{5, -1, -1, 1}, {-1, 4, 1, 1}, {4, 10, 1, -1}, {10, 5, -1, -1}};
 
@@ -58,11 +57,14 @@ public class BugLandRunner
 		{
 			for (int j = 0; j < 4 ; j++)
 			{
-				float r = rand.nextFloat(), g = rand.nextFloat(), b = rand.nextFloat();
+				int r = ThreadLocalRandom.current().nextInt(100, 256);
+				int g = ThreadLocalRandom.current().nextInt(0, 256);
+				int b = ThreadLocalRandom.current().nextInt(0, 256);
 				Rock tempRock = new Rock(new Color(r, g, b));
-				world.add(
-					new Location(rockPos[i][0] += rockPos[i][2], rockPos[i][1] += rockPos[i][3]),
+				
+				world.add(new Location(rockPos[i][0] += rockPos[i][2], rockPos[i][1] += rockPos[i][3]),
 					tempRock);
+					
 				rockList.add(tempRock);
 			}
 		}
@@ -77,13 +79,14 @@ public class BugLandRunner
 		fifthBug.act();
 		fifthBug.act();
 
-		// Retrieve direction and location of 5th bug
+		// Retrieve and print direction and location of 5th bug
 
 		int fifthBugDir = fifthBug.getDirection();
 		Location fifthBugLoc = fifthBug.getLocation();
+		
 		System.out.println("Direction: " + fifthBugDir + "\nLocation: " + fifthBugLoc);
 
-		// Show the world in GUI
+		// Show the world in the GUI
 		
 		world.show();
 	}
