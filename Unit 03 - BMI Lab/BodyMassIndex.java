2138dcca2d51872ba5c9eabc227141d96845ecd0
Kevin Meng <kevin.meng.01@gmail.com>
2018-09-17 15:51:36 -0500

    complete lab 3

--- Unit 03 - BMI Lab/BodyMassIndex.java
+++ Unit 03 - BMI Lab/BodyMassIndex.java
@@ -1,12 +1,18 @@
 // add your 6-item heading here 
 
-// any imports go here
+import java.util.Scanner;
 
 public class BodyMassIndex 
 {
 	// Define constants for unit conversions
 	
-	
+	public static final double CM_IN_M = 100.0;
+	public static final double KG_IN_LB = 0.453592;
+	public static final double CM_IN_IN = 2.54;
+
+	// Create global Scanner object
+
+	public static Scanner sc = new Scanner(System.in);
 	
 	// Do not make any changes to the main!
 	public static void main(String[] args) 
@@ -26,7 +32,19 @@ public class BodyMassIndex
 	 */
 	public static void calculateMetricBMI()
 	{
-		// complete for 80 pt version
+		int weightKG, heightCM;
+
+		System.out.print("Enter your weight in kilograms: ");
+		System.out.flush();
+		weightKG = sc.nextInt();
+
+		System.out.print("\nEnter your height in centimeters: ");
+		System.out.flush();
+		heightCM = sc.nextInt();
+
+		double bmi = getBMI(weightKG, heightCM);
+		System.out.printf("\nA body mass index of 20 - 25 is considered \"normal\"\n");
+		System.out.printf("Your BMI is %.2f\n\n", bmi);
 	} 
 		
 	
@@ -34,30 +52,44 @@ public class BodyMassIndex
 	 */ 
 	public static double getBMI(int weightKG, int heightCM)
 	{
-		// complete for 80 pt version
-		return 0;
+		return (double) weightKG / (heightCM / CM_IN_M) / (heightCM / CM_IN_M);
 	}
 	
 	/** 
 	 */
 	public static void calculateImperialBMI()
 	{
-		// complete for 100 pt version
+		int weightLB, heightIN;
+
+		System.out.print("Enter your weight in pounds: ");
+		System.out.flush();
+		weightLB = sc.nextInt();
+
+		System.out.print("\nEnter your height in inches: ");
+		System.out.flush();
+		heightIN = sc.nextInt();
+
+		int weightKG = convertPoundsToKG(weightLB);
+		int heightCM = convertInchesToCM(heightIN);
+
+		double bmi = getBMI(weightKG, heightCM);
+		System.out.printf("\nA body mass index of 20 - 25 is considered \"normal\"\n");
+		System.out.printf("Your BMI is %.2f\n\n", bmi);
+
+		sc.close();
 	}
 	
 	/** 
 	 */
 	public static int convertPoundsToKG(int numPounds)
 	{
-		// complete for 100 pt version
-		return 0;
+		return (int) Math.round((double) numPounds * KG_IN_LB);
 	}
 	
 	/** 
 	 */
 	public static int convertInchesToCM(int numInches)
 	{
-		// complete for 100 pt version
-		return 0;
+		return (int) Math.round((double) numInches * CM_IN_IN);
 	}
 }
