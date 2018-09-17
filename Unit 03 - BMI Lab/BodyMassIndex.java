5e03e30c72d9e0b04de327c4eb3cf95319030a14
Kevin Meng <kevin.meng.01@gmail.com>
2018-09-17 15:26:06 -0500

    create BMI lab

---
+++ Unit 03 - BMI Lab/BodyMassIndex.java
@@ -0,0 +1,63 @@
+// add your 6-item heading here 
+
+// any imports go here
+
+public class BodyMassIndex 
+{
+	// Define constants for unit conversions
+	
+	
+	
+	// Do not make any changes to the main!
+	public static void main(String[] args) 
+	{
+		// 80-point version
+		// Input user's weight and height in metric units and calculate BMI
+		calculateMetricBMI();
+		System.out.println();
+		
+		// 100-point version
+		// Input user's weight and height in imperial units and calculate BMI
+		calculateImperialBMI();
+		System.out.println();
+	}
+	
+	/** 
+	 */
+	public static void calculateMetricBMI()
+	{
+		// complete for 80 pt version
+	} 
+		
+	
+	/** 
+	 */ 
+	public static double getBMI(int weightKG, int heightCM)
+	{
+		// complete for 80 pt version
+		return 0;
+	}
+	
+	/** 
+	 */
+	public static void calculateImperialBMI()
+	{
+		// complete for 100 pt version
+	}
+	
+	/** 
+	 */
+	public static int convertPoundsToKG(int numPounds)
+	{
+		// complete for 100 pt version
+		return 0;
+	}
+	
+	/** 
+	 */
+	public static int convertInchesToCM(int numInches)
+	{
+		// complete for 100 pt version
+		return 0;
+	}
+}
