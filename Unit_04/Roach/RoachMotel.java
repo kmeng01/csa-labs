594b2604faaf413a637a575d091b4de0f1f04303
Kevin Meng <kevin.meng.01@gmail.com>
2018-10-01 15:41:33 -0500

    Update RoachMotel.java

--- Unit_04/Roach/RoachMotel.java
+++ Unit_04/Roach/RoachMotel.java
@@ -34,12 +34,14 @@ public class RoachMotel
 		// constructor, declare and instantiate a RoachPopulation object to
 		// represent the roach population in your kitchen. 
 		// Display initial population using the accessor method.
+		
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter starting population of the kitchen (an integer): ");
 		int initialPopulation = scan.nextInt();
 
 		RoachPopulation kitchenRoaches = new RoachPopulation(100);
-		System.out.printf("My kitchen has an initial population of %d roaches.\n\n", kitchenRoaches.getRoaches());
+		System.out.printf("My kitchen has an initial population of %d roaches.\n\n",
+			kitchenRoaches.getRoaches());
 
 		/*
 		 * 85-point version
@@ -60,21 +62,24 @@ public class RoachMotel
 		/*
 		 * 100-point version (uncomment section below)
 		 */
+		 
 		// Spray insecticide in the kitchen and display the results
+		
 		System.out.print("Enter strength of kitchen insecticide used (ex, 22.75): ");
 		double kitchenSpray = scan.nextDouble();
 		
-		int numKilled = kitchenRoaches.spray(kitchenSpray);
-		int numRemaining = kitchenRoaches.getRoaches();
-		System.out.printf("My kitchen now has %d roaches after killing %d roaches.\n\n", numRemaining, numKilled);
+		int numKilledKitchen = kitchenRoaches.spray(kitchenSpray);
+		System.out.printf("My kitchen now has %d roaches after killing %d roaches.\n\n",
+			kitchenRoaches.getRoaches(), numKilledKitchen);
 
 		// Spray insecticide in the lab and display the results
+		
 		System.out.print("Enter strength of lab insecticide used (ex, 22.75): ");
 		double labSpray = scan.nextDouble();
 		
-		numKilled = labRoaches.spray(labSpray);
-		numRemaining = labRoaches.getRoaches();
-		System.out.printf("My lab now has %d roaches after killing %d roaches.\n\n", numRemaining, numKilled);
+		int numKilledLab = labRoaches.spray(labSpray);
+		System.out.printf("My lab now has %d roaches after killing %d roaches.\n\n",
+			labRoaches.getRoaches(), numKilledLab);
 		
 	}
 }
