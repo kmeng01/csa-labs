367b53a7afdebcffb3d2ec6b6dc7aa440a77e7c7
Kevin Meng <kevin.meng.01@gmail.com>
2019-04-02 16:01:42 -0500

    complete car lab

--- Unit_12/CarFuel/Car.java
+++ Unit_12/CarFuel/Car.java
@@ -1,10 +1,23 @@
+/**
+ * The Car class provides the structure for keeping track of a vehicle's details,
+ * including odometer level, fuel level, etc.
+ *
+ * @author Kevin Meng
+ * Collaborators: None
+ * Teacher Name: Mrs. Ishman
+ * Period: 7
+ * Due Date: 4/3/2019
+ */
 
+import javax.xml.bind.annotation.XmlElement.DEFAULT;
 
 public class Car
 {
 	/** Default initialization values */
 	public static final int DEFAULT_YEAR = 1999;
 	public static final String DEFAULT_MAKE_MODEL = "Ford Pinto";
+	public static final double DEFAULT_CAPACITY = 14.5;
+	public static final int DEFAULT_MPG = 24;
 
 	/** Manages unique ID numbers for all cars */
 	private static int nextVIN = 1001;
@@ -21,12 +34,19 @@ public class Car
 	/** the current mileage on the car */
 	private double odometer;
 
+	private double gallons;
+	private double capacity;
+	private int mpg;
+
 	/** Constructs a Car with default settings
 	 */
 	public Car()
 	{
 		year = DEFAULT_YEAR;
 		makeAndModel = DEFAULT_MAKE_MODEL;
+		gallons = 0;
+		capacity = DEFAULT_CAPACITY;
+		mpg = DEFAULT_MPG;
 		vin = getNextVIN();
 		odometer = 0;
 	}
@@ -41,6 +61,29 @@ public class Car
 		this.year = year;
 		this.makeAndModel = makeAndModel;
 		this.odometer = odometer;
+		this.gallons = 0;
+		this.capacity = DEFAULT_CAPACITY;
+		this.mpg = DEFAULT_MPG;
+		this.vin = getNextVIN();
+	}
+
+	/**
+	 * Create a car with the 6 given parameters
+	 * @param year year manufactured
+	 * @param makeAndModel make and model of car
+	 * @param odometer current mileage
+	 * @param gallons number of gallons in fuel tank
+	 * @param capacity max capacity of tank
+	 * @param mpg miles per gallon efficiency of car
+	 */
+	public Car(int year, String makeAndModel, double odometer, double gallons, double capacity, int mpg)
+	{
+		this.year = year;
+		this.makeAndModel = makeAndModel;
+		this.odometer = odometer;
+		this.gallons = gallons;
+		this.capacity = capacity;
+		this.mpg = mpg;
 		this.vin = getNextVIN();
 	}
 
@@ -57,7 +100,43 @@ public class Car
 	 */
 	public void drive(double miles)
 	{
-		odometer += miles;
+		double needGas = miles / mpg;
+		if (gallons < needGas)
+		{
+			odometer += mpg * gallons;
+			gallons = 0;
+		}
+		else
+		{
+			gallons -= miles / mpg;
+			odometer += miles;
+		}
+	}
+
+	/**
+	 * Refill tank to max capacity
+	 * @return always returns true
+	 */
+	public boolean addFuel()
+	{
+		gallons = capacity;
+		return true;
+	}
+
+	/**
+	 * Adds amount of fuel if possible, otherwise fills tank to capacity
+	 * @param amt amount of fuel to add
+	 * @return true if adding amt does not overflow tank, otherwise false
+	 */
+	public boolean addFuel(double amt)
+	{
+		if (gallons + amt > capacity)
+		{
+			gallons = capacity;
+			return false;
+		}
+		gallons += amt;
+		return true;
 	}
 
 	/** Drives the car a given number of miles per day
@@ -86,13 +165,22 @@ public class Car
 		return vin;
 	}
 
+	/**
+	 * Returns number of gallons in tank
+	 * @return number of gallons in fuel tank
+	 */
+	public double getFuelLevel()
+	{
+		return gallons;
+	}
+
 	/** Provides this car's information in a string
 	 *  @return car info as a string
 	 */
 	@Override
 	public String toString()
 	{
-		return year + " " + makeAndModel + " " + vin + " " + String.format("%,.1f", odometer);
+		return year + " " + makeAndModel + " " + vin + " " + String.format("%,.1f %,.4f %,.3f %d", odometer, gallons, capacity, mpg);
 	}
 
 	/** Determines if 2 cars are the same based on its VIN
--- Unit_12/CarFuel/FuelTankTester.java
+++ Unit_12/CarFuel/FuelTankTester.java
@@ -1,4 +1,13 @@
-
+/**
+ * The FuelTankTester is the driver class for Car. It performs various operations
+ * on Car objects.
+ *
+ * @author Kevin Meng
+ * Collaborators: None
+ * Teacher Name: Mrs. Ishman
+ * Period: 7
+ * Due Date: 4/3/2019
+ */
 
 public class FuelTankTester
 {
@@ -9,32 +18,53 @@ public class FuelTankTester
 		// Display both cars using toString
 
 		Car car1 = new Car();
-		Car car2 = new Car();
+		Car car2 = new Car(2002, "Porsche 911", 1700);
 
+		System.out.println("Car 1: " + car1.toString());
+		System.out.println("Car 2: " + car2.toString() + "\n");
 
     	// Drive both cars. Display each car's fuel tank level and odometer 
     	// using accessor methods (not toString)
 		
-		
-		
-		
-		
-		
+		car1.drive(750);
+		car2.drive(5280);
+
+		System.out.printf("Car 1's odometer: %.1f\n", car1.getMileage());
+		System.out.printf("Car 1's fuel level: %.1f\n\n", car1.getFuelLevel());
+
+		System.out.printf("Car 2's odometer: %.1f\n", car2.getMileage());
+		System.out.printf("Car 2's fuel level: %.1f\n\n", car2.getFuelLevel());
 
     	// 100-pt Version:
     	
     	// Create a Car object using your 6-parameter constructor
     	// Display the car using toString
 	
-	
+		Car car3 = new Car(1951, "Hudson Hornet", 87293.8, 10, 18.5, 16);
+		System.out.println("Car 3: " + car3 + "\n");
 
     	// Drive your car and add fuel, displaying the results
     	// as directed on your lab sheet
 
+		car3.drive(100);
+		System.out.printf("After 100 miles: %s\n", car3.toString());
+		car3.drive(75);
+		System.out.printf("After 75 miles: %s\n", car3.toString());
+		car3.addFuel();
+		System.out.printf("After filling tank: %.1f\n\n", car3.getFuelLevel());
 
+		car3.drive(500);
+		System.out.printf("After 500 miles: %s\n\n", car3.toString());
 
+		double[] fuelAdd = {6.8, 11.7, 5.25};
 
+		for (int i = 0; i < fuelAdd.length; i++)
+		{
+			boolean success = car3.addFuel(fuelAdd[i]);
+			System.out.printf("Able to add %s gal? %b\n", fuelAdd[i], success);
+			System.out.printf("Current fuel level: %.1f\n", car3.getFuelLevel());
+		}
 
-
+		System.out.println("Car 3: " + car3.toString());		
     }
 }
